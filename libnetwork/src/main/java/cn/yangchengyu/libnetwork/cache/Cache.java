0dccb7c1d902228b66e7bc95e1d0f82c925ffdef
Chengyu Yang <chengyu_yang@outlook.com>
2020-03-29 13:32:27 +0800

    [feat]cache parcelable

--- libnetwork/src/main/java/cn/yangchengyu/libnetwork/cache/Cache.java
+++ libnetwork/src/main/java/cn/yangchengyu/libnetwork/cache/Cache.java
@@ -1,21 +1,53 @@
 package cn.yangchengyu.libnetwork.cache;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import androidx.annotation.NonNull;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
-import java.io.Serializable;
-
 @Entity(tableName = "cache")
-public class Cache implements Serializable {
+public class Cache implements Parcelable {
 
     /**
      * PrimaryKey 必须要有,且不为空,autoGenerate 主键的值是否由Room自动生成,默认false
      */
-    @PrimaryKey(autoGenerate = false)
     @NonNull
+    @PrimaryKey(autoGenerate = false)
     public String key = "";
 
     public byte[] data;
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.key);
+        dest.writeByteArray(this.data);
+    }
+
+    public Cache() {
+    }
+
+    protected Cache(Parcel in) {
+        this.key = in.readString();
+        this.data = in.createByteArray();
+    }
+
+    public static final Parcelable.Creator<Cache> CREATOR = new Parcelable.Creator<Cache>() {
+        @Override
+        public Cache createFromParcel(Parcel source) {
+            return new Cache(source);
+        }
+
+        @Override
+        public Cache[] newArray(int size) {
+            return new Cache[size];
+        }
+    };
 }
 
