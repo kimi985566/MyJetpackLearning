4cb68ee8f306cfe61fe34e1b9bbc5b93379532c2
Chengyu Yang <chengyu_yang@outlook.com>
2020-04-26 16:50:58 +0800

    [update]优化页面

--- app/src/main/java/cn/yangchengyu/myjetpacklearning/ui/home/fragment/HomeFragment.kt
+++ app/src/main/java/cn/yangchengyu/myjetpacklearning/ui/home/fragment/HomeFragment.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import cn.yangchengyu.libcommon.model.NetworkState
 import cn.yangchengyu.libnavannotation.FragmentDestination
 import cn.yangchengyu.myjetpacklearning.R
@@ -34,30 +37,48 @@ class HomeFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        homeViewModel =
-            ViewModelProviders.of(this, HomeFeedInjection.provideHomeViewModelFactory())
-                .get(HomeViewModel::class.java)
-
+        //设置ViewModel
+        initViewModel()
+        //设置Adapter
         initAdapter()
-
-        homeViewModel.repos.observe(viewLifecycleOwner, Observer {
-            adapter?.submitList(it)
-        })
-
-        homeViewModel.networkState.observe(viewLifecycleOwner, Observer { state ->
-            swipeRefresh.isRefreshing = NetworkState.LOADING == state
-        })
-
-        swipeRefresh?.setColorSchemeResources(R.color.colorPrimary)
-        swipeRefresh?.setOnRefreshListener {
-            homeViewModel.refresh()
-        }
-
-        homeViewModel.getFeedType("all")
+        //设置列表
+        initRecyclerView()
     }
 
     private fun initAdapter() {
         adapter = HomeFeedAdapter(context!!)
-        list.adapter = adapter
+    }
+
+    private fun initViewModel() {
+        homeViewModel = ViewModelProviders.of(this, HomeFeedInjection.provideHomeViewModelFactory())
+            .get(HomeViewModel::class.java)
+
+        //刷新数据
+        homeViewModel.getFeedType("all")
+
+        homeViewModel.repos.observe(viewLifecycleOwner, Observer {
+            adapter?.submitList(it)
+        })
+        homeViewModel.networkState.observe(viewLifecycleOwner, Observer { state ->
+            swipeRefresh?.isRefreshing = NetworkState.LOADING == state
+        })
+    }
+
+    private fun initRecyclerView() {
+        //设置Adapter
+        list?.adapter = adapter
+        //默认给列表中的Item 一个 10dp的ItemDecoration
+        context?.let { ctx ->
+            list?.addItemDecoration(DividerItemDecoration(ctx, LinearLayoutManager.VERTICAL).apply {
+                ContextCompat.getDrawable(ctx, R.drawable.list_divider)?.let { drawable ->
+                    setDrawable(drawable)
+                }
+            })
+        }
+        //设置刷新布局
+        swipeRefresh?.setColorSchemeResources(R.color.colorPrimary)
+        swipeRefresh?.setOnRefreshListener {
+            homeViewModel.refresh()
+        }
     }
 }
